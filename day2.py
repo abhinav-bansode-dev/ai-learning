1f5b89124e7779f9da2d5ef43c8a431fa094a3bb
Abhinav Bansode <abhinav.bansode.dev@gmail.com>
2026-06-01 19:19:49 +0530

    Day 2 code

---
+++ day2.py
@@ -0,0 +1,13 @@
+# Day 2: Python Basics
+skills = ["Python", "Git", "GitHub", "AI"]
+print(skills)
+
+for skill in skills:
+    print(skill)
+
+for i in range(1, 6):
+    print(i)
+
+tools = ["VS Code", "Git", "Python"]
+for tool in tools:
+    print("Tool:", tool)
