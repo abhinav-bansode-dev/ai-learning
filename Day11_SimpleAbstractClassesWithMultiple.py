f2bbbec0c469f003805fbf23628b7e201cc046c4
Abhinav Bansode <abhinav.bansode.dev@gmail.com>
2026-06-13 14:20:51 +0530

    Simple abstract class with multiple child classes

---
+++ Day11_SimpleAbstractClassesWithMultiple.py
@@ -0,0 +1,30 @@
+#Simple abstract classes with multiple child classes.
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark!")
+
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow!")
+
+
+class Cow(Animal):
+    def sound(self):
+        print("Moo!")
+
+
+animals = [Dog(), Cat(), Cow()]
+
+for animal in animals:
+    animal.sound()
