5198f361eb05c244c30279a754b5c3816f6dc0fd
Abhinav Bansode <abhinav.bansode.dev@gmail.com>
2026-08-20 17:52:11 +0530

    Gemini CrewAI test

---
+++ Gemini_CrewAI.py
@@ -0,0 +1,12 @@
+#Setup Gemini API
+
+"""import os
+from dotenv import load_dotenv
+from openai import api_key
+
+from crewai import Agent, Task, Crew #CrewAI
+
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api_key)"""
